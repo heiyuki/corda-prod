08de24783ec4bd93f751b6c2ad1cbd8ada040c0c
Khaled Romdhane <heiyukidev@gmail.com>
2017-04-10 10:29:26 +0100

    first commit

---
+++ runnodes.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Change to the directory of this script (%~dp0)
+Pushd %~dp0
+
+FOR /D %%G in (.\*) DO (
+ Pushd %%G
+ start java -jar corda.jar
+ start java -jar corda-webserver.jar
+ Popd
+)
+
+Popd
